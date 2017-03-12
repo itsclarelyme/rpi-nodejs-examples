010ce467ba299955556b04890deecf7986233442
itsclareleme <clarelee0106@gmail.com>
2017-03-11 21:21:30 -0800

    Added express server to turn on and off LED

--- led/led.js
+++ led/led.js
@@ -1,4 +1,7 @@
 var wpi = require('wiring-pi');
+var express = require('express')
+
+
 
 // GPIO pin of the led
 var configPin = 7;
@@ -8,10 +11,31 @@ var configTimeout = 1000;
 wpi.setup('wpi');
 wpi.pinMode(configPin, wpi.OUTPUT);
 
+const turnLed = function(state) {
+	wpi.digitalWrite(configPin, state);
+}
+
+/**
 var isLedOn = 0;
 
 setInterval(function() {
 	isLedOn = +!isLedOn;
 	//isLedOn = !isLedOn;
-	wpi.digitalWrite(configPin, isLedOn );
+	
 }, configTimeout);
+*/
+
+// <-- BEGIN EXPRESS -->
+var app = express()
+
+app.get('/turn_led_on', function (req, res) {
+  turnLed(1);
+})
+
+app.get('/turn_led_off', function (req, res) {
+  turnLed(0);
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
